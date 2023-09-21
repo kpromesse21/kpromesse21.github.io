19ed1a57adced131387584bc80a3a479bb662d8a
promesse kambale <kpromesse1@gmail.com>
2023-09-21 22:01:47 +0200

    clean code in application

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+// let test;
+
+// // test=test+1;
+// console.log(test);
+
+
+
+// let index;
+for (let index=0 ; index<10; index++) {
+   prompt("dans la boucle" , index);
+//    break;    
+}
+
